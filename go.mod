34ed7a574f62896dd3e084061999d8c386e7a876
Jesse Horner <jrhorner@pm.me>
2020-09-16 17:02:11 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jrhorner1/ookla-speedtest
+
+go 1.15
+
+require github.com/sirupsen/logrus v1.6.0
---
+++ speedtest.go
@@ -0,0 +1,100 @@
+package speedtest
+
+import (
+	"time"
+	"os/exec"
+	"bytes"
+	log "github.com/sirupsen/logrus"
+	"encoding/json"
+)
+
+type Speedtest struct {
+    Type string
+    Timestamp time.Time
+    Ping Ping
+    Download DataRate
+    Upload DataRate
+    PacketLoss float64
+    Isp string
+    Interface Interface
+    Server Server
+    Result Result
+}
+
+type Ping struct {
+	Jitter float64
+    Latency float64
+}
+
+type DataRate struct {
+	Bandwidth int
+	Bytes int
+	Elapsed int
+}
+
+type Interface struct {
+	InternalIp string
+	Name string
+	MacAddr string
+	IsVpn bool
+	ExternalIp string
+}
+
+type Server struct {
+	Id int
+	Name string
+	Location string
+	Country string
+	Host string
+	Port int
+	Ip string
+}
+
+type Result struct {
+	Id string
+	Url string
+}
+
+func RunSpeedtest() *Speedtest {
+	args := [2]string{"", ""}
+	return speedtest(args)
+}
+
+func RunSpeedtestWithServer(server string) *Speedtest {
+	args := [2]string{"-s", server}
+	return speedtest(args)
+}
+
+func RunSpeedtestWithHost(host string) *Speedtest {
+	args := [2]string{"-o", host}
+	return speedtest(args)
+}
+
+func speedtest(testargs [2]string) *Speedtest {
+	var test *exec.Cmd
+	if testargs[0] != "" && testargs[1] != "" {
+		test = exec.Command("speedtest", "-f", "json", testargs[0], testargs[1])
+	} else {
+		test = exec.Command("speedtest", "-f", "json")
+	}
+	var out bytes.Buffer
+	test.Stdout = &out
+
+	log.Info("Running Speedtest...")
+	err := test.Run()
+	if err != nil  {
+		log.Error("speedtest-cli error", err.Error())
+	}
+	log.Info("Speedtest completed..")
+
+	jsonOutput := out.Bytes()
+	log.Info(string(jsonOutput))
+
+	var results Speedtest
+	err = json.Unmarshal(jsonOutput, &results)
+	if err != nil {
+		log.Error("JSON unmarshal error", err.Error())
+	}
+	
+	return(&results)
+}
